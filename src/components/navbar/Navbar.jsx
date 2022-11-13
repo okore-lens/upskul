409697131d85373f94f8c7d0a4a04eefb5e3be39
lens <okorelens27@gmail.com>
2022-11-13 16:58:06 +0300

    Navbar modification

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./navbar.scss";
 
 const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 40) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+  });
+
   return (
     <nav>
-      <div className="logo">
-        <Link to="/">UpSkuL</Link>
-      </div>
-      <div className="middle-nav">
-        <Link to="/art">Art Corner</Link>
+      <div className={`container ${navbar ? "active" : ""}`}>
+        <div className="logo">
+          <Link to="/">UpSkuL</Link>
+        </div>
+        <div className="middle-nav">
+          <Link to="/art">Art Corner</Link>
 
-        <Link to="/memories">Memories</Link>
+          <Link to="/memories">Memories</Link>
 
-        <Link to="/products">Products</Link>
-      </div>
-      <div className="help">
-        <Link to="/about">
-          <button>Get In Touch</button>
-        </Link>
+          <Link to="/products">Products</Link>
+        </div>
+        <div className="help">
+          <Link to="/about">
+            <button>Get In Touch</button>
+          </Link>
+        </div>
       </div>
     </nav>
   );

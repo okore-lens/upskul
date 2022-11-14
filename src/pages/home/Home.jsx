6f8da5501bd4beeddd9291eafc351d75d9d93749
lens <okorelens27@gmail.com>
2022-11-14 22:38:13 +0300

    Home --> about section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
 import CircularSquare from "../../components/circularSquare/CircularSquare";
-import MediaHandle from "../../components/mediaHandles/MediaHandle";
-
-import "./home.scss";
 import Hero from "./components/Hero";
 import UpComing from "../../components/upComing/UpComing";
+import image from "../../assets/images/image.jpg";
+
+import "./home.scss";
 
 const Home = () => {
   return (
@@ -25,6 +25,42 @@ const Home = () => {
           <UpComing />
           <UpComing />
         </div>
+      </div>
+      <div className="home-about">
+        <div className="about">
+          <div className="left">
+            <h1>
+              Who Are <br />
+              We?
+            </h1>
+            <div className="circular-help">
+              <div className="inner-circle">
+                <p>Get In Touch</p>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="floating-div">
+              <img src={image} />
+            </div>
+            <div className="text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Doloribus doloremque facere nesciunt tempore officiis
+                necessitatibus ut deleniti neque numquam. Harum, eius fuga?
+                Temporibus ipsum maxime, neque explicabo fugit adipisci dolores,
+                repellat pariatur atque nesciunt commodi quidem distinctio
+                quisquam possimus quis!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
+                porro ipsa labore est libero. Accusantium debitis quas
+                temporibus assumenda. Repudiandae.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="image"></div>
       </div>
     </div>
   );

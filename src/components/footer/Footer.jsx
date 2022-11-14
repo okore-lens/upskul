789afb6587fa2188c40256defe4d5ff26e7c24c2
lens <okorelens27@gmail.com>
2022-11-14 23:18:28 +0300

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="Footer">
+      <div className="container">
+        <div className="legal">
+          <h1>Upskul</h1>
+          <ul>
+            <li>&copy; 2015-present</li>
+            <li>Privacy Policy</li>
+            <li>Terms and Conditions</li>
+          </ul>
+        </div>
+        <div className="products">
+          <h3>Products</h3>
+          <ul>
+            <li>Blog</li>
+            <li>Merchandise</li>
+          </ul>
+        </div>
+        <div className="features">
+          <h3>Features</h3>
+          <ul>
+            <li>Artists</li>
+            <li>Shows</li>
+          </ul>
+        </div>
+        <div className="company">
+          <h3>Company</h3>
+          <ul>
+            <li>About Us</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

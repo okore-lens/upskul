8456d28f0f32fa18732e0ed3b3160cc4355a6883
lens <okorelens27@gmail.com>
2022-11-14 23:52:14 +0300

    About Page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import HeroText from "./component/HeroText";
+import image from "../../assets/images/image.jpg";
+
+import "./about.scss";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="About">
+      <div className="hero">
+        <div className="right">
+          <img src={image} />
+        </div>
+        <div className="left">
+          <h1>About Upskul</h1>
+          <HeroText />
+          <HeroText />
+        </div>
+      </div>
+      <div className="section">
+        <div className="left">
+          <div className="circle">
+            <img src={image} />
+          </div>
+        </div>
+        <div className="right">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            laboriosam sint praesentium dignissimos consectetur eos autem velit
+            repudiandae. Voluptates quisquam qui itaque, obcaecati error ut
+            voluptatum blanditiis voluptas fuga in inventore labore eius, minima
+            illum deleniti. Nam obcaecati veritatis esse, quod magnam nihil
+            deserunt ut ea illo quibusdam laboriosam odit maiores, molestiae
+            recusandae molestias.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            laboriosam sint praesentium dignissimos consectetur eos autem velit
+            repudiandae. Voluptates quisquam qui itaque, obcaecati error ut
+            voluptatum blanditiis voluptas fuga in inventore labore eius, minima
+            illum deleniti. Nam obcaecati veritatis esse, quod magnam nihil
+            deserunt ut ea illo quibusdam laboriosam odit maiores, molestiae
+            recusandae molestias.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            laboriosam sint praesentium dignissimos consectetur eos autem velit
+            repudiandae. Voluptates quisquam qui itaque, obcaecati error ut
+            voluptatum blanditiis voluptas fuga in inventore labore eius, minima
+            illum deleniti. Nam obcaecati veritatis esse, quod magnam nihil
+            deserunt ut ea illo quibusdam laboriosam odit maiores, molestiae
+            recusandae molestias.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
---
+++ src/pages/about/component/HeroText.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const HeroText = () => {
+  return (
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem aliquam
+      sed minima tempore sapiente maiores facilis et id temporibus, ex veniam
+      velit corrupti fuga? Sed tenetur consectetur quo sunt nostrum praesentium
+      illo voluptates earum, dolorem nulla alias ipsa, laudantium itaque
+      similique aspernatur temporibus cum placeat. Officia accusantium ea
+      recusandae necessitatibus rerum perspiciatis eos exercitationem
+      reprehenderit! Alias, voluptates vero. Dolorem voluptates officia beatae
+      et, accusantium eius odit ea error ut magnam doloribus itaque id saepe non
+      magni aperiam possimus. Perspiciatis minus, ea officia dolorum delectus
+      magni nihil nam, ullam ratione dignissimos quia corporis quibusdam
+      maiores, laborum culpa beatae dolores iure odit provident voluptatem
+      dolore id voluptate temporibus consequuntur. Expedita officia ad quos
+      dolorem consequatur veritatis rerum corporis minus modi, voluptatem optio
+      earum in consequuntur quas architecto atque fugiat, ullam ex corrupti
+      commodi. Commodi vero, temporibus illum, voluptates earum consequuntur
+      fugiat eveniet tenetur expedita ad ea beatae fuga, veniam quam tempore.
+      Magni odio esse fugit tempora, nostrum nesciunt dolor aliquid, praesentium
+      omnis voluptates quis. Consequuntur quod fugit accusamus dolores tenetur
+      cupiditate quas explicabo obcaecati quaerat. Non quas, adipisci dolorem
+      reiciendisoluptatibus voluptatum, quidem corporis earum, ducimus sit
+      incidunt, ab nobis. Vitae minima saepe vero dolore natus, consequatur
+      accusantium quo reiciendis omnis, necessitatibus nihil quia hic ipsam
+      delectus in magnam deleniti eaque doloremque aliquid laudantium, corporis
+      dolorum enim eveniet? Magnam, itaque eaque commodi odio ipsa distinctio?
+      Architecto sit beatae qui rerum corrupti dicta, asperiores sequi.
+      Accusamus beatae doloremque nemo vitae, quis nesciunt ab, minus placeat
+      autem tempore, numquam mollitia molestiae? Autem? ipsum dolor sit amet
+      consectetur adipisicing elit. Vel earum ducimus nesciunt ratione quaerat
+      maxime aut quas voluptates. Magni minima omnis deserunt est autem cum
+      molestiae, nam culpa hic quis pariatur. Fuga ullam ab molestiae sed, quasi
+      recusandae ipsam dolorum maxime at, quibusdam nihil cupiditate, neque quam
+      magnam voluptas minima culpa non dolor laborum eaque ipsum.
+    </p>
+  );
+};
+
+export default HeroText;
